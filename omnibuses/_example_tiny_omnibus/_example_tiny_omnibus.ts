eea73850752ca25262d4f085d81d6f26cbd8b855
phonktown <phonktown@lido.fi>
2026-07-16 15:15:11 +0300

    fix: restore content drifted during commit re-signing

--- omnibuses/_example_tiny_omnibus/_example_tiny_omnibus.ts
+++ omnibuses/_example_tiny_omnibus/_example_tiny_omnibus.ts
@@ -1,7 +1,18 @@
 import { Omnibus } from "../../src/omnibuses";
+import { createContracts } from "../../src/contracts";
+
+import { Agent_ABI } from "../../abi/Agent.abi";
+import { Finance_ABI } from "../../abi/Finance.abi";
+import { MiniMeToken_ABI } from "../../abi/MiniMeToken.abi";
 
 const LDO_TRANSFER_AMOUNT = 10_000n * 10n ** 18n;
 const LDO_TRANSFER_RECIPIENT = "0x0000000000000000000000000000000000000777";
+
+const contracts = createContracts({
+  agent: [Agent_ABI, "0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"],
+  ldo: [MiniMeToken_ABI, "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32"],
+  finance: [Finance_ABI, "0xB9E5CBB9CA5b0d659238807E84D0176930753d86"],
+});
 
 export default Omnibus.create({
   network: "mainnet",
@@ -12,14 +23,16 @@ export default Omnibus.create({
   quorumReached: undefined,
 
   calls: ({ blueprints }) => [
-    blueprints.tokens.transferLDO({
+    blueprints.tokens.transfer(contracts, {
       title: "Transfer 10,000 LDO to test address",
       to: LDO_TRANSFER_RECIPIENT,
+      token: contracts.ldo.address,
       amount: LDO_TRANSFER_AMOUNT,
+      comment: "Transfer 10,000 LDO to test address",
     }),
   ],
 
-  testVote: async ({ client, checks, contracts, passOmnibus }) => {
+  testVote: async ({ client, checks, passOmnibus }) => {
     const [recipientBalanceBefore, agentBalanceBefore] = await Promise.all([
       client.read(contracts.ldo, "balanceOf", [LDO_TRANSFER_RECIPIENT]),
       client.read(contracts.ldo, "balanceOf", [contracts.agent.address]),
@@ -27,7 +40,15 @@ export default Omnibus.create({
 
     await passOmnibus();
 
-    await checks.tokens.checkLDOBalance(LDO_TRANSFER_RECIPIENT, recipientBalanceBefore + LDO_TRANSFER_AMOUNT);
-    await checks.tokens.checkLDOBalance(contracts.agent.address, agentBalanceBefore - LDO_TRANSFER_AMOUNT);
+    await checks.tokens.checkERC20Balance({
+      token: contracts.ldo.address,
+      account: LDO_TRANSFER_RECIPIENT,
+      expectedBalance: recipientBalanceBefore + LDO_TRANSFER_AMOUNT,
+    });
+    await checks.tokens.checkERC20Balance({
+      token: contracts.ldo.address,
+      account: contracts.agent.address,
+      expectedBalance: agentBalanceBefore - LDO_TRANSFER_AMOUNT,
+    });
   },
 });
